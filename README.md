49586724fbf7116c0a4169055e266c3ca89dbca0
linz <hnaynlin19@163.com>
2017-03-17 16:03:54 +0800

    上传代码
    快捷输入 css 相关图片

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# vscode-cssimg
-fast to get img info for css
+## vscode 插件 
+------------------
+在书写css 时 一键式 输入当前目录下的 图片的 尺寸 及 url
+
+f1 ---> call img --> 选择图片 
+
+生成以下代码
+```
+    width: 1920px;
+    height: 800px;
+    background-image:url(../img/common/header-bg.jpg);
+```
+快捷键
+```
+{
+        "key": "ctrl+shift+4",
+        "command": "linz.callImg"
+    }
+```
+
+仅查找 工作目录下  src 、 publuc 、assets 下所有图片
+目前没有写配置 需要的 可以自己到 src/util.ts 下修改
---
+++ src/extension.ts
@@ -0,0 +1,23 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {cssImg} from './util'
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "linz-onemore" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    var css = vscode.commands.registerCommand('linz.callImg',cssImg);
+    context.subscriptions.push(css);
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
